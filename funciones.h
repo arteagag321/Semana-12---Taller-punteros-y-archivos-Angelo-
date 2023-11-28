c31d17a9aaaf47551dff58d4ad676c6ed4421e0c
arteagag321 <152224437+arteagag321@users.noreply.github.com>
2023-11-27 20:45:11 -0500

    funciones.h

---
+++ funciones.h
@@ -0,0 +1,13 @@
+// funciones.h
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+int leerEnteroPositivo(char*);
+int leerEntero(char*);
+int leerEnteroEntre(char*, int, int);
+float leerFlotantePositivo(char*);
+float leerFlotante(char*);
+float leerFlotanteEntre(char*, int, int);
+float leerCarcaterPositivo(char*);
+
+#endif
